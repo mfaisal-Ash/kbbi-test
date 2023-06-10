4ebdee019cf002a6207937bd9f489c453b8826d6
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-06-10 22:26:01 +0700

    modified

--- ktbi-dja/wsgi.py
+++ ktbi-dja/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_kbbi project.
+WSGI config for ktbi-dja project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_kbbi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ktbi-dja.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
