5d543fd0ffcffd5a09399ae84c4cf07c8137a699
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-06-13 20:49:42 +0700

    updated 0.1

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-module.exports = {
-    cari: require('./src/ktbi.js')
-}
+const { Bot, InlineKeyboard } = require('grammy')
+const kbbi = require('kbbi-scraper');
 
+const bot = new Bot(process.env.token)
+
+bot.command('start', (ctx) => ctx.reply(process.env.start_text))
+
+bot.on('message', (ctx) => {
+    let query = ctx.message.text;
+    const keyboard = new InlineKeyboard().url('Lihat diweb kbbi', 'https://kbbi.kemdikbud.go.id/entri/'+query)                                                                                                                                                                
+    kbbi(query).then(res => {
+     ctx.reply('Kata: '+query+'\nDitemukan: '+res.data.arti, {
+        reply_markup: keyboard
+       })
+    }).catch(e => {
+         ctx.reply(e);
+   })
+})          
+
+console.log('Bot Mu sudah berjalan dengan baik')
+console.log('Jangan lupa subs @nekozu ya!')
+
+bot.start();
